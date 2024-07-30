991b4083012b41404f0614606ec0af8e8ae0f000
crimson666 <ankurbsws@gmail.com>
2024-07-30 08:31:52 +0530

    1st commit

---
+++ src/test/java/com/javaredis/spring_data_redis/SpringDataRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaredis.spring_data_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
