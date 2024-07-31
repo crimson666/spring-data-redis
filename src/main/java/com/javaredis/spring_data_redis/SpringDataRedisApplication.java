d2c79b81e9073260e8ef906077fead995f0c9d1b
crimson666 <ankurbsws@gmail.com>
2024-07-31 19:57:00 +0530

    Stuff

--- src/main/java/com/javaredis/spring_data_redis/SpringDataRedisApplication.java
+++ src/main/java/com/javaredis/spring_data_redis/SpringDataRedisApplication.java
@@ -1,10 +1,41 @@
 package com.javaredis.spring_data_redis;
 
+import com.javaredis.spring_data_redis.entity.Product;
+import com.javaredis.spring_data_redis.repository.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/product")
 public class SpringDataRedisApplication {
+
+	@Autowired
+	private ProductDao dao;
+
+	@PostMapping
+	public Product save(@RequestBody Product pct){
+		return dao.save(pct);
+	}
+
+	@GetMapping
+	public List<Product> getAllProduct(){
+		return dao.getAllRecord();
+	}
+
+	@GetMapping("/{id}")
+	public Product getById(@PathVariable int id){
+		return dao.findById(id);
+	}
+
+	@DeleteMapping("/{id}")
+	public String deleteById(@PathVariable int id){
+		return dao.delete(id);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataRedisApplication.class, args);
---
+++ src/main/java/com/javaredis/spring_data_redis/repository/ProductDao.java
@@ -0,0 +1,33 @@
+package com.javaredis.spring_data_redis.repository;
+
+import com.javaredis.spring_data_redis.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ProductDao {
+    @Autowired
+    private RedisTemplate template;
+    public static final String HASH_KEY_PRODUCT = "Product";
+
+    public Product save(Product product){
+        template.opsForHash().put(HASH_KEY_PRODUCT,product.getId(),product);
+        return product;
+    }
+    public List<Product> getAllRecord(){
+        return template.opsForHash().values(HASH_KEY_PRODUCT);
+    }
+
+    public Product findById(int Id){
+        return (Product) template.opsForHash().get(HASH_KEY_PRODUCT,Id);
+    }
+
+    public String delete(int Id){
+        template.opsForHash().delete(HASH_KEY_PRODUCT,Id);
+        return "Deletion done";
+    }
+
+}
